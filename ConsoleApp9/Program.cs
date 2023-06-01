8f1cfb71d489243124de09ee6713fdd36ee8c5ba
Polinka <Polinka@DESKTOP-MHBJDTO>
2023-06-01 16:52:16 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] mas = new int[10];
+Random rnd = new Random();
+int temp = 0;
+for (int i = 0; i <10; i++)
+{
+    mas[i] = rnd.Next(1, 100); 
+    Console.Write(mas[i] + " ");
+    if (mas[i] % 2 == 0)
+        temp++;
+}
+Console.WriteLine();
+Console.WriteLine("Количество четных чисел в массиве: "+ temp);
